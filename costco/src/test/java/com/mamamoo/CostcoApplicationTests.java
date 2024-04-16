0ba55df3622f48a85a8800bb69ffd58ca9b08b04
chojaehu <fhfh5741@gmail.com>
2024-04-16 15:09:01 +0900

    first

---
+++ costco/src/test/java/com/mamamoo/CostcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamamoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
