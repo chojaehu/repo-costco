34767ffcd0eec5f95641a7ad3dcaf085cfcdd2d4
ramses <ramses69@naver.com>
2024-04-16 16:05:16 +0900

    기본구성 upload

--- costco/src/main/java/com/mamamoo/CostcoApplication.java
+++ costco/src/main/java/com/mamamoo/CostcoApplication.java
@@ -2,8 +2,9 @@ package com.mamamoo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class CostcoApplication {
 
 	public static void main(String[] args) {
