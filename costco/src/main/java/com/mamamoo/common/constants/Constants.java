dc71d2ff0665c0567bc965125e9cf9771a5e2127
ramses <ramses69@naver.com>
2024-04-18 14:36:36 +0900

    constants:경로 추가

--- costco/src/main/java/com/mamamoo/common/constants/Constants.java
+++ costco/src/main/java/com/mamamoo/common/constants/Constants.java
@@ -1,13 +1,11 @@
 package com.mamamoo.common.constants;
 
 public class Constants {
-	public static final String PATH_CODE 		= "infra/v1/code/";
-	public static final String PATH_CODE_GROUP 	= "infra/v1/codegroup/";
 	public static final String PATH_HOME 		= "infra/v1/home/";
 	public static final String PATH_LOGIN 		= "infra/v1/login/";
-	public static final String PATH_MEMBER 		= "infra/v1/member/";
-	public static final String PATH_PRODUCT 	= "infra/v1/product/";
-	public static final String PATH_TEMP 		= "infra/v1/temp/";
+	public static final String PATH_MYORDER 	= "infra/v1/myorder/";
+	public static final String PATH_WAREHOUSING	= "infra/v1/warehousing/";
+	public static final String PATH_ORDERS 		= "infra/v1/orders/";
 	
 	public static final String DATETIME_FORMAT_BASIC = "yyyy-MM-dd HH:mm:ss";
 	
