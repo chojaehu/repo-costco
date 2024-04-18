73f332f07ca69bb7fcb91d4ddae7a905d819c533
ramses <ramses69@naver.com>
2024-04-18 14:43:49 +0900

    경로수정

--- costco/src/main/java/com/mamamoo/common/constants/Constants.java
+++ costco/src/main/java/com/mamamoo/common/constants/Constants.java
@@ -1,6 +1,8 @@
 package com.mamamoo.common.constants;
 
 public class Constants {
+	public static final String PATH_CODE		= "infra/v1/code/";
+	public static final String PATH_CODE_GROUP  = "infra/v1/codegroup/";
 	public static final String PATH_HOME 		= "infra/v1/home/";
 	public static final String PATH_LOGIN 		= "infra/v1/login/";
 	public static final String PATH_MYORDER 	= "infra/v1/myorder/";
