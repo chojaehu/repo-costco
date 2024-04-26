7de46897ebee601b0be300f4c5861be242c54925
ramses <ramses69@naver.com>
2024-04-26 15:55:23 +0900

    모달폼:fnModalCaptionChange 수정

--- costco/src/main/resources/static/js/modal.js
+++ costco/src/main/resources/static/js/modal.js
@@ -1,14 +1,16 @@
-
-// 모달폼 띄우기
-fnModalFormOneBtnShow = function(paramTitle, paramContent) {
-	$("#title").html(paramTitle);
-	$("#modalFormOneBtnContent").html(paramContent);
-	$("#modalFormOneBtn").modal('show');
-}
-
 // 모달폼 내용 수정
-fnModalCaption = function(paramTitle, paramContent, paramButton) {
-	$("#title").html(paramTitle);
-	$("#content").html(paramContent);
-	$("#btnName1").html(paramButton);
+fnModalCaptionChange = function(paramIndex, paramTitle, paramContent) {
+	if(paramIndex == 0) {
+		// 삭제외 경우
+		$("#btnCancel").html("확인");
+		$("#title").html(paramTitle);
+		$("#contents").html(paramContent);		
+		$("#lineDelete").css("display", "none");
+	} else {
+		// 삭제하는 경우
+		$("#btnCancel").html("취소");
+		$("#title").html("삭제");
+		$("#contents").html("삭제를 진행 하시겠습니까?");
+		$("#lineDelete").css("display", "");
+	}
 }
