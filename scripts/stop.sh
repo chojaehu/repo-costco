f423a203efec15953bde6e6e6f0ab45f48b7666a
chojaehu <155616667+chojaehu@users.noreply.github.com>
2024-05-07 11:17:01 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,7 +2,7 @@
 # stop.sh
 
 PROJECT="costco-0.0.1"
-PROJECT_FULL_PATH_01="/home/ec2-user/service/ezticket-prdt-01"
+PROJECT_FULL_PATH_01="/home/ec2-user/service/costco-prdt-01"
 JAR_FILE="$PROJECT_FULL_PATH_01/$PROJECT.jar"
 LOG_PATH="$PROJECT_FULL_PATH_01/logs"
 
